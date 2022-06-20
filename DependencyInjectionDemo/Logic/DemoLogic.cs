538b0ff72ca99c283eb591240ca93b78971ae11a
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-06-20 23:48:03 +0600

    tight coupled DemoLogic

---
+++ DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,12 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class DemoLogic
+{
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+    public DemoLogic()
+    {
+        Value1 = Random.Shared.Next(0, 1001);
+        Value2 = Random.Shared.Next(0, 1001);
+    }
+}
